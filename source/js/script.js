155f13181f999ac1036b02a3f9a07b047bbfac0f
WinterInside <de_animator@mail.ru>
2021-11-27 20:37:14 +0300

    layout desctop quiz

--- source/js/script.js
+++ source/js/script.js
@@ -1,32 +1,10 @@
 $(document).ready(function(){
 	$('.slider').slick({
-		arrows:true,
-		dots:false,
-		slidesToShow:4,
+		arrows:false,
+		dots:true,
+		slidesToShow:1,
 		autoplay:true,
 		speed:1000,
-		autoplaySpeed:5000,
-		responsive:[
-			{
-				breakpoint: 1370,
-				settings: {
-					slidesToShow:3
-				}
-			},
-      {
-				breakpoint: 1070,
-				settings: {
-					slidesToShow:2
-				}
-			},
-			{
-				breakpoint: 760,
-				settings: {
-					slidesToShow:1,
-		      dots:true,
-          arrows:false,
-				}
-			}
-		]
+		autoplaySpeed:5000		
 	});
 });
